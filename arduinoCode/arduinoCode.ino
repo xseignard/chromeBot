49c42981e7a9d9b7d0b7e55af95e641b855cc463
xseignard <xavier.seignard@gmail.com>
2013-10-30 11:32:34 +0100

    Arduino Uno and Leonardo compatibility

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,14 +1,27 @@
 const int LED = 13;
 
 void setup() {
+#if defined (__AVR_ATmega32U4__)
+  // arduino leonardo
+  Serial1.begin(9600);
+#elif defined(__AVR_ATmega328P__)
+  // arduino uno
   Serial.begin(9600);
+#endif
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // data received
+#if defined (__AVR_ATmega32U4__)
+  // arduino leonardo
+  if (Serial1.available() > 0) {
+    int i = Serial1.read();
+#elif defined(__AVR_ATmega328P__)
+  // arduino uno
   if (Serial.available() > 0) {
     int i = Serial.read();
+#endif
     // the value maps to a direction
     switch (i) {
       // forward/left
